4b95a2a8b6e07e53cf8d105ad757529660ec25c1
yschiangg <16698828+yschiangg@users.noreply.github.com>
2022-04-19 21:06:13 -0500

    Add About section

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutCrumbl =()=>{
+  return(
+        <p>Crumbl monitors web requests and records all domains a tab communicates with, then calculate the total number of cookies that the relevant domains store on your browser. 
+          <br></br>Learn more about cookies on <a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank">Wikipedia: HTTP COOKIES</a>.</p>
+  )
+}
+
+export default AboutCrumbl;
